ce447126a474cb1b5b059e50914455bbb3a484cc
Druva <ch.dhruvakumar@gmail.com>
2020-02-08 03:49:27 +0530

    p5 script

---
+++ sketch.js
@@ -0,0 +1,16 @@
+function setup() {
+  createCanvas(400, 400);
+    background(220);
+}
+
+function draw() {
+
+  line(0,0,400,400);
+  strokeWeight(5);
+  stroke(0,255,0);
+  fill(0,0,255);
+  rect(mouseX,100,200,200);
+  fill(255,0,255);
+  noStroke();
+  ellipse(250,300,100,100);
+}
